4707d8ac7f3ef179fa8e8890c9c12896204f7070
Ben brady <Ben.Brady1989@gmail.com>
2022-01-04 15:42:13 -0500

    Portfolio cu
    Cleaned up extra to show code

--- Class Roster/degreeProgram.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <string>
-using namespace std;
-
-/*C.  Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE.*/
-enum degreeProgram { SECURITY, NETWORK, SOFTWARE };
